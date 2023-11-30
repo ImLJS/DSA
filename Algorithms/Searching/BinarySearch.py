07bccd6dc4790b146eb56b54ea6921bc44df41fd
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-11-30 12:23:58 +0530

    Binary Search

--- Algorithms/Searching/BinarySearch.py
+++ Algorithms/Searching/BinarySearch.py
@@ -6,6 +6,9 @@
 
 # Binary search is a searching algorithm that searches a target value within a sorted array. It compares the target
 # value to the middle element of the array. If they are not equal, the half in which the target cannot lie is
+# eliminated and the search continues on the remaining half, again taking the middle element to compare to the target
+# value, and repeating this until the target value is found. If the search ends with the remaining half being empty,
+# the target is not in the array.
 
 
 def binary_search(arr, key):
