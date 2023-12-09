a1f56d4f12ee0eb737ce318a7d25965d4787e1a3
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-12-09 17:33:40 +0530

    Strings

---
+++ Strings/string_operations.py
@@ -0,0 +1,23 @@
+# String Operations
+
+# Concatenation
+print("Hello" + "World")
+
+# Repetition
+print("Hello" * 3)
+
+# Indexing
+print("Hello"[0])
+
+# Slicing
+print("Hello"[1:3])
+
+# Length
+print(len("Hello"))
+
+# Iteration
+for letter in "Hello":
+    print(letter)
+
+# Membership
+print("H" in "Hello")
