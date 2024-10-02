6ac7e687f3fd8c1951ae870c03500ff5b64679a2
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-10-02 11:09:40 +0530

    Chore: Update DFS and BFS to include cyclic graphs

--- Graphs/Traversal/BreadthFirstSearch.py
+++ Graphs/Traversal/BreadthFirstSearch.py
@@ -8,18 +8,44 @@
 from collections import deque
 
 
+# For Acyclic Graphs
 def bfs(graph, source):
-    stack = deque([source])
+    queue = deque([source])  # Initialize the queue with the source node
 
-    while stack:
-        node = stack.popleft()
+    while queue:
+        node = queue.popleft()  # Pop the first element from the queue
         print(node)
-        for neighbour in graph[node]:
-            stack.append(neighbour)
+        for neighbour in graph[node]:  # Iterate over the neighbours of the node
+            queue.append(neighbour)  # Add the neighbour to the queue
+
+
+# For Cyclic Graphs
+
+
+def bfs(graph, source):
+    queue = deque([source])  # Initialize the queue with the source node
+    visited = set([source])  # Initialize the visited set with the source node
+
+    while queue:
+        node = queue.popleft()  # Pop the first element from the queue
+        print(node)
+        for neighbour in graph[node]:  # Iterate over the neighbours of the node
+            if neighbour not in visited:  # If the neighbour is not visited
+                queue.append(neighbour)  # Add the neighbour to the queue
+                visited.add(neighbour)  # Add the neighbour to the visited set
 
 
 graph = {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": ["f"], "e": [], "f": []}
+graph_cyclic = {
+    "a": ["b", "c"],
+    "b": ["d"],
+    "c": ["e"],
+    "d": ["f"],
+    "e": ["a"],
+    "f": [],
+}
 
 source = "a"
 
 bfs(graph, source)  # a b c d e f
+bfs(graph_cyclic, source)  # a b c d e f
--- Graphs/Traversal/DepthFirstSearch.py
+++ Graphs/Traversal/DepthFirstSearch.py
@@ -8,7 +8,8 @@
 from collections import deque
 
 
-def dfs(graph, start):
+# For Acyclic Graphs
+def dfs_acyclic(graph, start):
     stack = deque([start])
 
     while stack:
@@ -18,7 +19,48 @@ def dfs(graph, start):
             stack.append(neighbour)
 
 
+def dfs_acyclic_recursive(graph, node):
+    print(node)
+    for neighbour in graph[node]:
+        dfs_acyclic_recursive(graph, neighbour)
+
+
+# For Cyclic Graphs
+
+
+def dfs_cyclic(graph, start):
+    stack = deque([start])
+    visited = set([start])
+
+    while stack:
+        node = stack.pop()
+        print(node)
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                stack.append(neighbour)
+                visited.add(neighbour)
+
+
+def dfs_cyclic_recursive(graph, node, visited):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs_cyclic_recursive(graph, neighbour, visited)
+
+
 graph = {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": ["f"], "e": [], "f": []}
+graph_cyclic = {
+    "a": ["b", "c"],
+    "b": ["d"],
+    "c": ["e"],
+    "d": ["f"],
+    "e": ["a"],
+    "f": [],
+}
 
 source = "a"
-dfs(graph, source)  # a c e b d f
+dfs_acyclic(graph, source)  # a c e b d f
+dfs_acyclic_recursive(graph, source)  # a b d f c e
+dfs_cyclic(graph_cyclic, source)  # a c e b d f
+dfs_cyclic_recursive(graph_cyclic, source, set())  # a b d f c e
