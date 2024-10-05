ecc79bba55de1924d53c8e3d06a8f3b59a65e3f5
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-10-05 08:36:35 +0530

    Q. Largest Component

---
+++ Graphs/Questions/largestComponent.py
@@ -0,0 +1,48 @@
+# Q. Largest Component
+
+# Given an undirected graph, find the size of the largest connected component.
+
+# Example
+
+# Input:
+# graph = {"a": ["b", "c"], "b": ["a"], "c": ["a"], "d": ["e"], "e": ["d"]}
+# Output:
+# 3
+
+
+def largest_component(
+    graph,
+):  # O(V + E) time, O(V) space where V is the number of vertices and E is the number of edges
+    maxSize = 0  # Initialize the maximum size of the connected component
+    visit = set()  # Initialize the set to store the visited nodes
+
+    for node in graph:
+        size = dfs(graph, node, visit)  # Call the dfs function
+        maxSize = max(
+            size, maxSize
+        )  # Update the maximum size of the connected component
+
+    return maxSize  # Return the maximum size of the connected component
+
+
+def dfs(graph, node, visit):
+
+    if node in visit:  # If the node is already visited
+        return 0
+
+    visit.add(node)  # Add the node to the visited set
+
+    size = 1  # Initialize the size of the connected component
+    for neighbour in graph[node]:
+        size += dfs(
+            graph, neighbour, visit
+        )  # Recursively call the function and update the size
+
+    return size  # Return the size of the connected component
+
+
+graph = {0: [4, 7], 1: [], 2: [], 3: [6], 4: [0], 6: [3], 7: [0], 8: []}
+print(largest_component(graph))  # 3
+
+graph2 = {0: [8, 1, 5], 1: [0], 5: [0, 8], 8: [0, 5], 2: [3, 4], 3: [2, 4], 4: [3, 2]}
+print(largest_component(graph2))  # 4
