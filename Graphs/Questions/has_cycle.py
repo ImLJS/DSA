b54b394aecc1d876bef77dfe845298bf83f7ab52
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-10-11 17:30:17 +0530

    Add Questions - Has Cycle and Prereqs Possible

---
+++ Graphs/Questions/has_cycle.py
@@ -0,0 +1,20 @@
+# has cycle
+#
+# Write a function, has_cycle, that takes in an object representing the adjacency list
+# of a directed graph. The function should return a boolean indicating whether the
+# graph contains a cycle.
+
+# Example:
+
+# has_cycle({
+#   "a": ["b"],
+#   "b": ["c"],
+#   "c": ["a"],
+# }) # -> True
+
+# has_cycle({
+#   "a": ["b", "c"],
+#   "b": ["c"],
+#   "c": ["d"],
+#   "d": [],
+# }) # -> False
