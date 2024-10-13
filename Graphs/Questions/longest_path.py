f416451dde811cb03910a13555d7d0770f10d153
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-10-13 08:44:35 +0530

    Q. Longest Path

--- Graphs/Questions/longest_path.py
+++ Graphs/Questions/longest_path.py
@@ -27,3 +27,39 @@
 # }
 #
 # longest_path(graph) # -> 4
+
+
+def longest_path(graph):  # O(V+E)
+    def dfs(node):
+        if node in cache:  # memoization
+            return cache[node]
+        if not graph[node]:  # base case
+            return 0
+        max_path = 0
+        for neighbor in graph[node]:
+            max_path = max(max_path, dfs(neighbor) + 1)
+        cache[node] = max_path
+        return max_path
+
+    cache = {}
+    maxpath = 0
+    for node in graph:
+        maxpath = max(maxpath, dfs(node))
+    return maxpath
+
+
+graph = {"a": ["c", "b"], "b": ["c"], "c": []}
+
+graph2 = {
+    "a": ["c", "b"],
+    "b": ["c"],
+    "c": [],
+    "q": ["r"],
+    "r": ["s", "u", "t"],
+    "s": ["t"],
+    "t": ["u"],
+    "u": [],
+}
+
+print(longest_path(graph))  # -> 2
+print(longest_path(graph2))  # -> 4
