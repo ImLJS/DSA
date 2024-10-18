c8784e6555636a5c3dfebfba40cfcf7c45428fcb
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-10-18 06:56:45 +0530

    Add Comments to Prims Algorithm

--- Graphs/Algorithms/prims.py
+++ Graphs/Algorithms/prims.py
@@ -24,31 +24,39 @@ from collections import defaultdict
 
 
 def prims(graph):
-    V = len(graph)
-    key = [float("inf")] * V
-    parent = [None] * V
-    mstSet = [False] * V
-    key[0] = 0
-    parent[0] = -1
+    V = len(graph)  # Number of vertices
+    key = [float("inf")] * V  # Key values used to pick minimum weight edge in cut
+    parent = [None] * V  # Array to store constructed MST
+    mstSet = [False] * V  # To represent set of vertices included in MST
+    key[0] = 0  # Make key 0 so that this vertex is picked as first vertex
+    parent[0] = -1  # First node is always root of MST
 
     for _ in range(V):
-        u = minKey(key, mstSet)
-        mstSet[u] = True
+        u = minKey(
+            key, mstSet
+        )  # Pick the minimum key vertex from the set of vertices not yet included in MST
+        mstSet[u] = True  # Add the picked vertex to the MST Set
         for v in range(V):
-            if graph[u][v] and not mstSet[v] and graph[u][v] < key[v]:
-                key[v] = graph[u][v]
-                parent[v] = u
+            if (
+                graph[u][v] and not mstSet[v] and graph[u][v] < key[v]
+            ):  # Update key value and parent index of the adjacent vertices of the picked vertex
+                key[v] = graph[u][
+                    v
+                ]  # Update the key only if graph[u][v] is smaller than key[v]
+                parent[v] = u  # Set the parent of vertex v to u
 
-    return parent
+    return parent  # Return the constructed MST
 
 
 def minKey(key, mstSet):
-    min = float("inf")
-    min_index = -1
+    min = float("inf")  # Initialize min value
+    min_index = -1  # Initialize min_index
     for v in range(len(key)):
-        if key[v] < min and not mstSet[v]:
-            min = key[v]
-            min_index = v
+        if (
+            key[v] < min and not mstSet[v]
+        ):  # Pick the minimum key vertex from the set of vertices not yet included in MST
+            min = key[v]  # Update the min value
+            min_index = v  # Update the min_index
     return min_index
 
 
@@ -62,9 +70,9 @@ graph = [
     [0, 5, 7, 9, 0],
 ]
 
-parent = prims(graph)
-for i in range(1, len(parent)):
-    print(parent[i], "-", i, ":", graph[i][parent[i]])
+parent = prims(graph)  # Construct the MST
+for i in range(1, len(parent)):  # Print the constructed MST
+    print(parent[i], "-", i, ":", graph[i][parent[i]])  # Print the edge and its weight
 
 # Output:
 
