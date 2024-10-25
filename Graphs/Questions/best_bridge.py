a8fafd5c2fc6a09d2ab79134532f9f907280507f
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-10-25 20:23:21 +0530

    Q. Best Bridge

--- Graphs/Questions/best_bridge.py
+++ Graphs/Questions/best_bridge.py
@@ -27,3 +27,52 @@
 #   ["W", "W", "W", "W", "W"],
 # ]
 # best_bridge(grid) # -> 2
+
+
+def best_bridge(grid):
+
+    def dfs(grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "L":
+            return
+        grid[i][j] = "W"
+        islands[-1].append((i, j))
+        for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            dfs(grid, i + di, j + dj)
+
+    islands = [[]]
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "L":
+                dfs(grid, i, j)
+                islands.append([])
+
+    island1, island2 = islands[0], islands[1]
+    min_bridge = float("inf")
+    for i1, j1 in island1:
+        for i2, j2 in island2:
+            min_bridge = min(min_bridge, abs(i1 - i2) + abs(j1 - j2) - 1)
+
+    return min_bridge
+
+
+grid = [
+    ["W", "W", "W", "L", "L"],
+    ["L", "L", "W", "W", "L"],
+    ["L", "L", "L", "W", "L"],
+    ["W", "L", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"],
+]
+
+print(best_bridge(grid))  # -> 1
+
+grid = [
+    ["W", "W", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"],
+    ["L", "L", "W", "W", "L"],
+    ["W", "L", "W", "W", "L"],
+    ["W", "W", "W", "L", "L"],
+    ["W", "W", "W", "W", "W"],
+]
+
+print(best_bridge(grid))  # -> 2
